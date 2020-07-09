205c7d20a86cc8417c0a90c028b3c27e5d3a503c
Ken Peters <peterskw@verizon.net>
2020-07-09 13:59:48 -0400

    Plot1

--- Plot1.R
+++ Plot1.R
@@ -3,7 +3,7 @@ df<-read.table("household_power_consumption.txt", header = T,
 dataplott<- subset(df,df$Date=="1/2/2007"|df$Date=="2/2/2007")
 datetime <- strptime(paste(dataplott$Date, dataplott$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 hist(dataplott$Global_active_power, col = "red", main = "Global Active Power",
-     xlab = "Global Acitive Power (kilowatts", ylab = "Frequency")
+     xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
 dev.copy(png, file = "plot1.png", width = 480, height = 480)
 dev.off()
 
